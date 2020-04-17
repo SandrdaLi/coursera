d7bcaab59d1f6524750ebcc93b32e772ff3ce733
SandrdaLi <mysandboxes@gmail.com>
2020-04-17 16:10:14 -0400

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -10,3 +10,22 @@ def read_employees(csv_file_location):
 
 employee_list = read_employees('/home/student-04-8b2d0ed74e60/data/employees.csv')
 print(employee_list)
+
+def process_data(employee_list):
+  department_list = []
+  for employee_data in employee_list:
+    department_list.append(employee_data['Department'])
+  department_data = {}
+  for department_name in set(department_list):
+    department_data[department_name] = department_list.count(department_name)
+  return department_data
+
+dictionary = process_data(employee_list)
+print(dictionary)
+
+def write_report(dictionary, report_file):
+  with open(report_file, "w+") as f:
+    for k in sorted(dictionary):
+      f.write(str(k)+':'+str(dictionary[k])+'\n')
+    f.close()
+write_report(dictionary, '/home/student-04-8b2d0ed74e60/test_report.txt')
